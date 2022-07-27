954b0f36f3db69a4b84ad60dc3b88aa3a7789e28
ComunicacionLobos <comunicacion@miprepanogales.mx>
2022-07-27 11:52:50 -0700

    cambio a php

---
+++ templates/header.php
@@ -0,0 +1,20 @@
+<div class="container d-flex align-items-center px-4">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+				aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item"><a href="index.html" class="nav-link pl-0">Inicio</a></li>
+					<li class="nav-item "><a href="about.html" class="nav-link">Nuestro Campus</a></li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						  Comunidad
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+						  <a class="dropdown-item" href="administrativos.html">Personal Administrativo</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
