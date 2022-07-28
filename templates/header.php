2e642afc4d84dc50d3ae0e86018a2565298fe0f4
ComunicacionLobos <comunicacion@miprepanogales.mx>
2022-07-27 17:02:03 -0700

    la mesa

--- templates/header.php
+++ templates/header.php
@@ -14,7 +14,7 @@
 						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 						  <a class="dropdown-item" href="administrativos.php">Personal Administrativo</a>
 						  <a class="dropdown-item" href="colosio.php">Plantel Colosio</a>
-						  <a class="dropdown-item" href="administrativos.php">Plantel La Mesa</a>
+						  <a class="dropdown-item" href="mesa.php">Plantel La Mesa</a>
 						</div>
 					</li>
 					<li class="nav-item dropdown">
