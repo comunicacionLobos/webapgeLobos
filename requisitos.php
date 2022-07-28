1686503230c3313060f1e09578844227616170f1
ComunicacionLobos <comunicacion@miprepanogales.mx>
2022-07-28 12:26:26 -0700

    fotos innecesarioas

--- requisitos.php
+++ requisitos.php
@@ -1,196 +1,202 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
   <title>Preparatoria Municipal</title>
-	<meta charset="utf-8">
-	<link rel="icon" href="images/logo1.png" type="image/icon type">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
-    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+  <meta charset="utf-8">
+  <link rel="icon" href="images/logo1.png" type="image/icon type">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/aos.css">
+  <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+  <link rel="stylesheet" href="css/animate.css">
 
-    <link rel="stylesheet" href="css/ionicons.min.css">
-    
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-  <body>
-  <?php  require_once('templates/header.php'); ?>
-    <!-- END nav -->
-    
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text align-items-center justify-content-center">
-          <div class="col-md-9 ftco-animate text-center">
-            <h1 class="mb-2 bread">Requisitos para inscribirse</h1>
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="css/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/aos.css">
+
+  <link rel="stylesheet" href="css/ionicons.min.css">
+
+  <link rel="stylesheet" href="css/flaticon.css">
+  <link rel="stylesheet" href="css/icomoon.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+  <?php require_once('templates/header.php'); ?>
+  <!-- END nav -->
+
+  <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+          <h1 class="mb-2 bread">Requisitos para inscribirse</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- ======= Frequently Asked Questions Section ======= -->
+  <section id="faq" class="faq">
+    <div class="container" data-aos="fade-up">
+      <div class="section-header">
+        <h2>Documentos en Original y Copia</h2>
+      </div>
+      <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="200">
+        <div class="col-lg-10">
+
+          <div class="accordion accordion-flush" id="faqlist">
+
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  Acta de Nacimiento
+                </button>
+              </h3>
+              <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  Acta de Nacimiento
+                </div>
+              </div>
+            </div><!-- # Faq item-->
+
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  Certificado de secundaria o constancia de terminación
+                </button>
+              </h3>
+              <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  Certificado de secundaria o constancia de terminación
+                </div>
+              </div>
+            </div><!-- # Faq item-->
+
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  Cuatro fotografías tamaño infantil en blanco y negro
+                </button>
+              </h3>
+              <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  Cuatro fotografías tamaño infantil en blanco y negro
+                </div>
+              </div>
+            </div><!-- # Faq item-->
+
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  CURP
+                </button>
+              </h3>
+              <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  CURP
+                </div>
+              </div>
+            </div><!-- # Faq item-->
+
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  Tipo de Sangre
+                </button>
+              </h3>
+              <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  Tipo de Sangre
+                </div>
+              </div>
+            </div><!-- # Faq item-->
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  Numero del seguro social del alumno (IMSS)
+                </button>
+              </h3>
+              <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  Numero del seguro social del alumno (IMSS)
+                </div>
+              </div>
+            </div><!-- # Faq item-->
+            <div class="accordion-item">
+              <h3 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                  <i class="bi bi-question-circle question-icon"></i>
+                  INE del padre o tutor
+                </button>
+              </h3>
+              <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                <div class="accordion-body">
+                  INE del padre o tutor
+                </div>
+              </div>
+            </div><!-- # Faq item-->
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 
-        <!-- ======= Frequently Asked Questions Section ======= -->
-        <section id="faq" class="faq">
-          <div class="container" data-aos="fade-up"> 
-            <div class="section-header">
-              <h2>Documentos en Original y Copia</h2>
-            </div>   
-            <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="200">
-              <div class="col-lg-10">
-    
-                <div class="accordion accordion-flush" id="faqlist">
-    
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        Acta de Nacimiento
-                      </button>
-                    </h3>
-                    <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        Acta de Nacimiento
-                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-    
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        Certificado de secundaria o constancia de terminación
-                      </button>
-                    </h3>
-                    <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        Certificado de secundaria o constancia de terminación
-                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-    
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        Cuatro fotografías tamaño infantil en blanco y negro
-                      </button>
-                    </h3>
-                    <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        Cuatro fotografías tamaño infantil en blanco y negro
-                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-    
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        CURP
-                      </button>
-                    </h3>
-                    <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        CURP                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-    
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        Tipo de Sangre
-                      </button>
-                    </h3>
-                    <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        Tipo de Sangre
-                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        Numero del seguro social del alumno (IMSS)
-                      </button>
-                    </h3>
-                    <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        Numero del seguro social del alumno (IMSS)
-                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-                  <div class="accordion-item">
-                    <h3 class="accordion-header">
-                      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
-                        <i class="bi bi-question-circle question-icon"></i>
-                        INE del padre o tutor
-                      </button>
-                    </h3>
-                    <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                      <div class="accordion-body">
-                        INE del padre o tutor
-                      </div>
-                    </div>
-                  </div><!-- # Faq item-->
-                </div>   
+  <section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/edificio5.jpg);" data-stellar-background-ratio="0.5">
+    <div class="container">
+      <div class="row justify-content-center mb-5 pb-2 d-flex">
+        <div class="col-md-6 align-items-stretch d-flex">
+          <div class="img img-video d-flex align-items-center">
+            <div class="video justify-content-center">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/_nv2iAkxebo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 heading-section heading-section-white ftco-animate pl-lg-5 pt-md-0 pt-5">
+          <h2 class="mb-4">Colegiatura Cuatrimestral</h2>
+          <p>Somos una preparatoria incorporada a la maxima casa de estudios de Sonora UNISON </p>
+        </div>
+      </div>
+      <div class="row d-md-flex align-items-center justify-content-center">
+        <div class="col-lg-12">
+          <div class="row d-md-flex align-items-center">
+            <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+              <div class="block-18">
+                <div class="icon"><span class="flaticon-doctor"></span></div>
+                <div class="text">
+                  <strong class="number" data-number="1600">0</strong>
+                  <span>Costo (pesos)</span>
+                </div>
               </div>
             </div>
           </div>
-        </section>
-
-      		<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(images/edificio5.jpg);" data-stellar-background-ratio="0.5">
-    	<div class="container">
-    		<div class="row justify-content-center mb-5 pb-2 d-flex">
-    			<div class="col-md-6 align-items-stretch d-flex">
-    				<div class="img img-video d-flex align-items-center">
-    					<div class="video justify-content-center">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/_nv2iAkxebo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-							</div>
-    				</div>
-    			</div>
-          <div class="col-md-6 heading-section heading-section-white ftco-animate pl-lg-5 pt-md-0 pt-5">
-            <h2 class="mb-4">Colegiatura Cuatrimestral</h2>
-            <p>Somos una preparatoria incorporada a la maxima casa de estudios de Sonora UNISON </p>
-          </div>
-        </div>	
-    		<div class="row d-md-flex align-items-center justify-content-center">
-    			<div class="col-lg-12">
-    				<div class="row d-md-flex align-items-center">
-		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
-		            <div class="block-18">
-		            	<div class="icon"><span class="flaticon-doctor"></span></div>
-		              <div class="text">
-		                <strong class="number" data-number="1600">0</strong>
-		                <span>Costo (pesos)</span>
-		              </div>
-		            </div>
-		          </div>	         
-	          </div>
-          </div>
         </div>
-    	</div>
-    </section>
-	
-    <footer class="ftco-footer ftco-bg-dark ftco-section">
-      <?php  require_once('templates/footer.php'); ?>
-    </footer>
-    
-  
+      </div>
+    </div>
+  </section>
+
+  <footer class="ftco-footer ftco-bg-dark ftco-section">
+    <?php require_once('templates/footer.php'); ?>
+  </footer>
+
+
 
   <!-- loader -->
-  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+      <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+      <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00" />
+    </svg></div>
 
 
   <script src="js/jquery.min.js"></script>
@@ -208,6 +214,7 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-    
-  </body>
+
+</body>
+
 </html>
