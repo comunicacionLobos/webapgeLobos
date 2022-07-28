92e7fca6eb094fc45c8ec22d6e5489e5c740a7e0
ComunicacionLobos <comunicacion@miprepanogales.mx>
2022-07-28 10:52:05 -0700

    maestros

--- index.php
+++ index.php
@@ -259,63 +259,72 @@
 				<div class="col-md-6 col-lg-3 ftco-animate">
 					<div class="staff">
 						<div class="img-wrap d-flex align-items-stretch">
-							<div class="img align-self-stretch" style="background-image: url(images/teacher-2.jpg);">
+							<div class="img align-self-stretch" style="background-image: url(images/gilbertomaestro.jpg);">
 							</div>
 						</div>
 						<div class="text pt-3 text-center">
-							<h3>Mitch Parker</h3>
-							<span class="position mb-2">English Teacher</span>
-							<div class="faded">
-								<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-								<ul class="ftco-social text-center">
-									<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-								</ul>
-							</div>
+							<h3>Gilberto Quijada</h3>
+							<span class="position mb-2">Maestro</span>							
 						</div>
 					</div>
 				</div>
 				<div class="col-md-6 col-lg-3 ftco-animate">
 					<div class="staff">
 						<div class="img-wrap d-flex align-items-stretch">
-							<div class="img align-self-stretch" style="background-image: url(images/teacher-3.jpg);">
+							<div class="img align-self-stretch" style="background-image: url(images/tmayramaestra.jpg);">
 							</div>
 						</div>
 						<div class="text pt-3 text-center">
-							<h3>Stella Smith</h3>
-							<span class="position mb-2">Art Teacher</span>
-							<div class="faded">
-								<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-								<ul class="ftco-social text-center">
-									<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-								</ul>
-							</div>
+							<h3>Mayra Noriega</h3>
+							<span class="position mb-2">Maestra</span>							
 						</div>
 					</div>
 				</div>
 				<div class="col-md-6 col-lg-3 ftco-animate">
 					<div class="staff">
 						<div class="img-wrap d-flex align-items-stretch">
-							<div class="img align-self-stretch" style="background-image: url(images/teacher-4.jpg);">
+							<div class="img align-self-stretch" style="background-image: url(images/kendormaestro.jpg);">
 							</div>
 						</div>
 						<div class="text pt-3 text-center">
-							<h3>Monshe Henderson</h3>
-							<span class="position mb-2">Science Teacher</span>
-							<div class="faded">
-								<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-								<ul class="ftco-social text-center">
-									<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-								</ul>
+							<h3>Kendor O’Shelly</h3>
+							<span class="position mb-2">Maestro</span>							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 ftco-animate">
+					<div class="staff">
+						<div class="img-wrap d-flex align-items-stretch">
+							<div class="img align-self-stretch" style="background-image: url(images/carolinamaestra.jpg);">
 							</div>
+						</div>
+						<div class="text pt-3 text-center">
+							<h3>Karolina Dummer</h3>
+							<span class="position mb-2">Maestra</span>							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 ftco-animate">
+					<div class="staff">
+						<div class="img-wrap d-flex align-items-stretch">
+							<div class="img align-self-stretch" style="background-image: url(images/aragonmaestro.jpg);">
+							</div>
+						</div>
+						<div class="text pt-3 text-center">
+							<h3>Jesus Aragon</h3>
+							<span class="position mb-2">Maestro</span>							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 col-lg-3 ftco-animate">
+					<div class="staff">
+						<div class="img-wrap d-flex align-items-stretch">
+							<div class="img align-self-stretch" style="background-image: url(images/irismaestra.jpg);">
+							</div>
+						</div>
+						<div class="text pt-3 text-center">
+							<h3>Iris Torrecillas</h3>
+							<span class="position mb-2">Maestra</span>							
 						</div>
 					</div>
 				</div>
