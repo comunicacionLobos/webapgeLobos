8a1312669f25cc94e980b8255d61e763e1c29e86
ComunicacionLobos <comunicacion@miprepanogales.mx>
2022-07-28 10:56:51 -0700

    ricarde

--- administrativos.php
+++ administrativos.php
@@ -165,6 +165,27 @@
 							</div>
 						</div>
 					</div>
+				</div>				
+				<div class="col-md-6 col-lg-3 ftco-animate">
+					<div class="staff">
+						<div class="img-wrap d-flex align-items-stretch">
+							<div class="img align-self-stretch" style="background-image: url(images/teacher-8.jpg);">
+							</div>
+						</div>
+						<div class="text pt-3 text-center">
+							<h3>Ricarde Perez</h3>
+							<span class="position mb-2">Jefe de la jefa del Esteban</span>
+							<div class="faded">
+								<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
+								<ul class="ftco-social text-center">
+									<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+									<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+									<li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
+									<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="col-md-6 col-lg-3 ftco-animate">
 					<div class="staff">
@@ -175,27 +196,6 @@
 						<div class="text pt-3 text-center">
 							<h3>Katty Amparano</h3>
 							<span class="position mb-2">Directora General</span>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-6 col-lg-3 ftco-animate">
-					<div class="staff">
-						<div class="img-wrap d-flex align-items-stretch">
-							<div class="img align-self-stretch" style="background-image: url(images/teacher-8.jpg);">
-							</div>
-						</div>
-						<div class="text pt-3 text-center">
-							<h3>Monshe Henderson</h3>
-							<span class="position mb-2">Science Teacher</span>
-							<div class="faded">
-								<p>I am an ambitious workaholic, but apart from that, pretty simple person.</p>
-								<ul class="ftco-social text-center">
-									<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-google-plus"></span></a></li>
-									<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-								</ul>
-							</div>
 						</div>
 					</div>
 				</div>
