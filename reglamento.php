438b5fb369365c983c414c342acd58103ced1359
ComunicacionLobos <comunicacion@miprepanogales.mx>
2022-08-03 16:58:55 -0700

    sfsf

--- reglamento.php
+++ reglamento.php
@@ -27,15 +27,12 @@
 <body>
   <?php  require_once('templates/header.php'); ?>
 
-  <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+  <section class="hero-wrap hero-wrap-2" style="background-image: url('images/colosio1.jpg');">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text align-items-center justify-content-center">
         <div class="col-md-9 ftco-animate text-center">
-          <h1 class="mb-2 bread">Certified Teacher</h1>
-          <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
-                  class="ion-ios-arrow-forward"></i></a></span> <span>Teacher <i
-                class="ion-ios-arrow-forward"></i></span></p>
+          <h1 class="mb-2 bread">Reglamento Escolar</h1>
         </div>
       </div>
     </div>
